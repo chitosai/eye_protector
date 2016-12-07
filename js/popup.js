e148d63ccd0a806fca53258015fe485ecfd9e378
TheC <i@thec.me>
2016-12-07 17:58:03 +0800

    重做popup.html

--- js/popup.js
+++ js/popup.js
@@ -1,70 +1,56 @@
 function onclick() {
-    var self = $(this),
-        key = self.attr('id'),
-        value = self.data('value');
+    var key = this.id;
 
-    // 反转选项
-    value = value ? false : true;
-
-    if( value ) {
-        self.addClass('checked');
-    } else {
-        self.removeClass('checked');
-    }
-    self.data('value', value);
-
-    // 根据选项类型保存
-    if( self.hasClass('unique') ) {
-        if( value ) {
-            option[key].push(host);
-        } else {
+    // 根据选项类型反转选项
+    var op = option[key];
+    if( typeof op == 'object' ) {
+        if( op.indexOf(host) > -1 ) {
             option[key].remove(host);
+        } else {
+            option[key].push(host);
         }
-    } else if ( self.hasClass('global') ) {
-        // 提示需要刷新生效
-        $('#menu .hint').show();
-        option[key] = value;
+    } else {
+        option[key] = !op;
     };
-
     saveOption();
+
+    // toggle class
+    this.classList.toggle('checked');
 }
 
 function init() {
     // 获取当前激活tab的域名
     chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs){
         url = tabs[0].url, host = getHost(url);
-
         if( !host ) {
-            console.log('无法获取当前激活的标签页域名');
+            alert('无法获取当前页面的域名');
         }
     });
 
     // 读取设置
     readOption(function() {
-        var i, item;
-        for(i in option) {
+        var key, item;
+        for(key in option) {
             // object类的设置是域名列表，检查当前域名是否在列表中
-            if( typeof option[i] == 'object' ) {
-                item = option[i];
+            if( typeof option[key] == 'object' ) {
+                item = option[key];
                 if( item.indexOf(host) > -1 ) {
-                    $('#'+i).addClass('checked').data('value', true);
-                } else {
-                    $('#'+i).data('value', false);
+                    $(key).classList.add('checked');
                 }
             } else {
             // bool
-                item = $('#'+i);
-                if( item.length ) {
-                    if( option[i] == true ) {
-                        item.addClass('checked');
-                    }
-                    item.data('value', option[i]);
+                item = $(key);
+                if( item && option[key] ) {
+                    item.classList.add('checked');
                 }
             }
         }
     });
 
-    $('#menu').on('click', '.item', onclick);
+    var nodes = $$('.option-item');
+    nodes.forEach(function(node) {
+        node.addEventListener('click', onclick);
+    });
 }
 
 init();
