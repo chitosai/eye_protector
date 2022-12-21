d8da9192e575b1abf65a5ca7e861a1a62e0af731
TheC <i@thec.me>
2022-12-21 12:04:48 -0800

    re-format

--- js/option.options.js
+++ js/option.options.js
@@ -1,65 +1,65 @@
 var CLICKLISTENERS = {
-  check: function() {
+  check: function () {
     var key = this.id;
 
     // 根据选项类型反转选项
     var option = OPTIONS.basic[key];
-    if( typeof option == 'boolean' ) {
+    if (typeof option == "boolean") {
       OPTIONS.basic[key] = !option;
-    };
+    }
     saveOption();
 
     // toggle class
-    this.classList.toggle('checked');
+    this.classList.toggle("checked");
   },
-  radio: function() {
-    var key = this.getAttribute('name'),
-	      val = this.id,
-	      nodes = $$('.item[name=' + key + ']');
+  radio: function () {
+    var key = this.getAttribute("name"),
+      val = this.id,
+      nodes = $$(".item[name=" + key + "]");
 
-    nodes.forEach(function(node) {
-      node.classList.remove('checked');
+    nodes.forEach(function (node) {
+      node.classList.remove("checked");
     });
-    this.classList.add('checked');
+    this.classList.add("checked");
 
     OPTIONS.basic[key] = this.id;
     saveOption();
   },
-  pickColor: function() {
-  	var color = this.value.trim().replace(/[^0-9a-fA-F]/g, ''),
-        preview = $('color-preview'),
-				btn = $('color-save');
+  pickColor: function () {
+    var color = this.value.trim().replace(/[^0-9a-fA-F]/g, ""),
+      preview = $("color-preview"),
+      btn = $("color-save");
     // 确保color中不包含非0-9a-fA-F的字符，且长度为3或6
-  	if( color == this.value && (color.length == 3 || color.length == 6) ) {
-	  	preview.style.backgroundColor = '#' + color;
-  		btn.disabled = false;
-  	} else {
-  		preview.style.backgroundColor = '#fff';
-  		btn.disabled = true;
-  	}
-  },
-  saveColor: function() {
-    var color = $('color').value.trim().toUpperCase();
-    if( color.length == 3 ) {
-      color = [color[0], color[0], color[1], color[1], color[2], color[2]].join('');
+    if (color == this.value && (color.length == 3 || color.length == 6)) {
+      preview.style.backgroundColor = "#" + color;
+      btn.disabled = false;
+    } else {
+      preview.style.backgroundColor = "#fff";
+      btn.disabled = true;
     }
-    OPTIONS.basic.replaceBgWithColor = '#' + color;
-    saveOption(function() {
-      $('color-save-success').style.display = 'block';
+  },
+  saveColor: function () {
+    var color = $("color").value.trim().toUpperCase();
+    if (color.length == 3) {
+      color = [color[0], color[0], color[1], color[1], color[2], color[2]].join("");
+    }
+    OPTIONS.basic.replaceBgWithColor = "#" + color;
+    saveOption(function () {
+      $("color-save-success").style.display = "block";
     });
   },
-  restoreColor: function() {
+  restoreColor: function () {
     var color = OPTIONS.basic.defaultBgColor;
     OPTIONS.basic.replaceBgWithColor = color;
-    saveOption(function() {
-      $('color-preview').style.backgroundColor = color;
-      $('color').value = color.slice(1);
+    saveOption(function () {
+      $("color-preview").style.backgroundColor = color;
+      $("color").value = color.slice(1);
     });
-  }
-}
+  },
+};
 
 function onRangeInput(e) {
-  $('current-brightness').textContent = this.value;
+  $("current-brightness").textContent = this.value;
 }
 
 function onRangeChange(e) {
@@ -72,43 +72,44 @@ function init() {
   i18n();
 
   // 读取设置
-  readOption(function() {
+  readOption(function () {
     var options = OPTIONS.basic,
-	      key, node;
-    for(key in options) {
-      if( options[key] === true ) {
+      key,
+      node;
+    for (key in options) {
+      if (options[key] === true) {
         node = $(key);
-        if(node) {
-          node.classList.add('checked');
+        if (node) {
+          node.classList.add("checked");
         }
       }
     }
 
     // 工作模式暂时没想到怎么做通用，就做个特例吧
-    $(OPTIONS.basic.mode).classList.add('checked');
+    $(OPTIONS.basic.mode).classList.add("checked");
 
     // 背景色
-    $('color-preview').style.backgroundColor = OPTIONS.basic.replaceBgWithColor;
-    $('color').value = OPTIONS.basic.replaceBgWithColor.slice(1);
+    $("color-preview").style.backgroundColor = OPTIONS.basic.replaceBgWithColor;
+    $("color").value = OPTIONS.basic.replaceBgWithColor.slice(1);
 
     // 亮度阈值
-    $('brightness').value = OPTIONS.basic.bgColorBrightnessThreshold;
+    $("brightness").value = OPTIONS.basic.bgColorBrightnessThreshold;
   });
 
   // 修改设置
-  var nodes = $$('.item');
-  nodes.forEach(function(node) {
-    node.addEventListener('click', CLICKLISTENERS[node.getAttribute('type')]);
+  var nodes = $$(".item");
+  nodes.forEach(function (node) {
+    node.addEventListener("click", CLICKLISTENERS[node.getAttribute("type")]);
   });
 
   // 修改背景色
-  $('color').addEventListener('input', CLICKLISTENERS.pickColor);
-  $('color-save').addEventListener('click', CLICKLISTENERS.saveColor);
-  $('color-restore').addEventListener('click', CLICKLISTENERS.restoreColor);
+  $("color").addEventListener("input", CLICKLISTENERS.pickColor);
+  $("color-save").addEventListener("click", CLICKLISTENERS.saveColor);
+  $("color-restore").addEventListener("click", CLICKLISTENERS.restoreColor);
 
   // 亮度阈值
-  $('brightness').addEventListener('input', onRangeInput);
-  $('brightness').addEventListener('change', onRangeChange);
+  $("brightness").addEventListener("input", onRangeInput);
+  $("brightness").addEventListener("change", onRangeChange);
 
   // options页面在打开的情况下，如果用户切到其他tabs修改了OPTIONS，这边需要及时更新
   // 不然可能出现options页面打开太久，OPTIONS中的list已经不是最新的，然后options触发了一次saveOptions造成
--- js/option.popup.js
+++ js/option.popup.js
@@ -1,9 +1,9 @@
 var url, host;
 
 function onClick() {
-  var list = OPTIONS[this.id+'List'];
-      
-  if( list.indexOf(host) > -1 ) {
+  var list = OPTIONS[this.id + "List"];
+
+  if (list.indexOf(host) > -1) {
     list.remove(host);
   } else {
     list.push(host);
@@ -11,42 +11,42 @@ function onClick() {
   saveOption();
 
   // toggle class
-  this.classList.toggle('checked');
+  this.classList.toggle("checked");
 }
 
 function init() {
   i18n();
 
   // 获取当前激活tab的域名
-  chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
-    url = tabs[0].url, host = getHost(url);
-    if( !host ) {
-      $('normal').style.display = 'none';
-      $('error').textContent = _('strProtocolMsg');
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    (url = tabs[0].url), (host = getHost(url));
+    if (!host) {
+      $("normal").style.display = "none";
+      $("error").textContent = _("strProtocolMsg");
       return;
     }
 
     // 读取当前域名的配置
-    readOption(function() {
+    readOption(function () {
       var mode = OPTIONS.basic.mode,
-          list = OPTIONS[mode + 'List'],
-          btn = $(mode);
+        list = OPTIONS[mode + "List"],
+        btn = $(mode);
 
       // 显示当前模式
-      $('mode').textContent = ( mode == 'positive' ? _('strPositive') : _('strPassive') );
-      $('mode').href = chrome.runtime.getURL('/options.html');
+      $("mode").textContent = mode == "positive" ? _("strPositive") : _("strPassive");
+      $("mode").href = chrome.runtime.getURL("/options.html");
 
       // 根据运行模式产生按钮状态
-      btn.style.display = 'block';
-      if( list.indexOf(host) > -1 ) {
-        btn.classList.add('checked');
+      btn.style.display = "block";
+      if (list.indexOf(host) > -1) {
+        btn.classList.add("checked");
       }
-    })
+    });
   });
 
   // 修改设置
-  $$('.item').forEach(function(node) {
-    node.addEventListener('click', onClick);
+  $$(".item").forEach(function (node) {
+    node.addEventListener("click", onClick);
   });
 }
 
